66ebaa0e1b7060134090a97cf57b143a73e106e4
SumitGh0sh <sumiitjee2024@gmail.com>
2026-05-04 12:18:48 +0530

    herosection with navbar

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,19 +1,34 @@
-import React from "react";
-import zenitsu from "../assets/zenitsu.mp4"
+import React from "react"
+import hero from "../assets/hero.mp4"
 
 export default function LandingPage() {
     return (
-        <div className="flex relative">
+        <div className="relative min-h-screen overflow-hidden">
             <video
-                src={zenitsu}
-                class="fixed top-0 left-0 w-screen h-screen object-cover -z-10"
-                autoplay
+                src={hero}
+                className="absolute inset-0 w-full h-full object-cover -z-10"
+                style={{ transformOrigin: 'center' }}
+                autoPlay
                 loop
                 muted
-                playsinline>
+                playsInline
+                preload="auto"
+            />
+            <div className="relative z-10 flex flex-col items-center justify-center h-screen text-white text-center px-4">
+                <img src="https://cdn-icons-png.flaticon.com/512/5968/5968705.png" alt="Logo" className="w-20 mb-4" />
+                <div className="absolute top-10 right-5 bg-[#c167ff] opacity-25 w-[60%] h-[60px] rounded-full flex items-center justify-evenly px-6 ">
+                    <div>Home</div>
+                    <div>Gallery</div>
+                    <div>event</div>
+                    <div>committee</div>
+                    <div>sponsor</div>
+                    <div>contact</div>
+                    <div className="text-white font-bold bg-[#c167ff] rounded-full px-4 py-2">register</div>
 
-            </video>
+                </div>
+            </div>
 
         </div>
     )
-};
+}
+
