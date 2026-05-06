784bb7ff76a6e33b65fe6333d44549cbf204deaa
Abhijeet <ghoshabhijit1295@gmail.com>
2026-05-06 13:21:17 +0530

    guest imgb upt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ import img7 from '../assets/gallaryImages/7.jpeg';
 import img8 from '../assets/gallaryImages/8.jpeg';
 import imgLogo from '../assets/logo.png';
 import imgPicon from '../assets/picon.png';
-import celeb from '../assets/celeb.webp';
+import celeb from '../assets/image.png';
 import imgRvs from '../assets/RVS_Logo_Coloured_White_bg.png';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -285,7 +285,7 @@ const About = () => {
               ref={badgeRef}
               className="absolute -top-6 -right-6 z-20 bg-gradient-to-r from-purple-600 to-pink-600 px-6 py-3 rounded-full text-sm font-bold shadow-xl text-white"
             >
-              Our Guest
+              Our Guest - Pranav Sharma
             </div>
           </div>
 
